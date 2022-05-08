298d0c3f4aa6a197cf883a93ffa5f8735629f20f
Berkcan Altungöz <61842758+BerkcanAltungoz@users.noreply.github.com>
2022-05-08 23:51:59 +0300

    Spring Boot Initial

---
+++ src/main/java/com/example/ecrow/ECrowApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecrow/ECrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
